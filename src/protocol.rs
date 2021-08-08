4ec422eeb20a5b7af43bcf0ed6dada4bf2d3debf
Zichen Liu <zichen.liu@shopee.com>
2021-08-08 20:14:02 +0800

    finished protocol & self echo test

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,183 +1,72 @@
-use std::collections::HashMap;
-use std::fs;
-use std::path::{Path, PathBuf};
-use std::sync::Arc;
-use std::time::{Duration, Instant};
+use bytes::Bytes;
+use std::io::{self, Read, Write};
+use std::os::unix::net::UnixStream;
 use std::usize;
 
-use async_channel::{bounded, Receiver, Sender};
-use bytes::Bytes;
-use tokio::net::UnixListener;
-use tokio::sync::{oneshot, Mutex};
+const LENGTH_TASK_FLAG: usize = 2;
+const LENGTH_TASK_BATCH: usize = 2;
+const LENGTH_TASK_ID: usize = 4;
+const LENGTH_TASK_BODY_LEN: usize = 4;
 
-#[derive(Debug, Clone, Copy)]
-pub enum TaskCode {
-    UnknownError,
-    Normal,
-    ValidationError,
-    InternalError,
-}
-
-#[derive(Debug, Clone)]
-pub struct Info {
-    pub code: TaskCode,
-    pub data: Bytes,
-}
+pub const FLAG_OK: u16 = 1; // 200
+pub const FLAG_BAD_REQUEST: u16 = 2; // 400
+pub const FLAG_VALIDATION_ERROR: u16 = 4; // 422
+pub const FLAG_INTERNAL_ERROR: u16 = 8; // 500
 
 #[derive(Debug)]
-struct Task {
-    info: Info,
-    notifier: oneshot::Sender<()>,
-    create_at: Instant,
-}
-
-impl Task {
-    pub fn new(data: Bytes, notifier: oneshot::Sender<()>) -> Self {
-        Task {
-            notifier,
-            create_at: Instant::now(),
-            info: Info {
-                code: TaskCode::UnknownError,
-                data,
-            },
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Processor {
-    tasks: Arc<Mutex<TaskHub>>,
-    batch_size: u32,
-    listener: UnixListener,
-    receiver: Receiver<usize>,
-    sender: Sender<usize>,
-}
-
-impl Processor {
-    fn new(
-        tasks: Arc<Mutex<TaskHub>>,
-        batch_size: u32,
-        path: &PathBuf,
-        receiver: Receiver<usize>,
-        sender: Sender<usize>,
-    ) -> Self {
-        println!("listen on {:?}", path);
-        let listener = UnixListener::bind(path).unwrap();
-        Processor {
-            tasks,
-            batch_size,
-            listener,
-            receiver,
-            sender,
-        }
-    }
-
-    async fn run(&self) {
-        loop {
-            let tasks_clone = self.tasks.clone();
-            let input_clone = self.receiver.clone();
-            let output_clone = self.sender.clone();
-            match self.listener.accept().await {
-                Ok((stream, addr)) => {
-                    println!("Accepted connection from {:?}", addr);
-                    tokio::spawn(async move {
-                        loop {
-                            let task_id = input_clone.recv().await;
-                            match task_id {
-                                Ok(task_id) => {
-                                    let tasks = tasks_clone.lock().await;
-                                    let task = tasks.table.get(&task_id);
-                                }
-                                Err(_) => {
-                                    break;
-                                }
-                            }
-                        }
-                    });
-                }
-                Err(e) => {
-                    eprintln!("Error accepting connection: {:?}", e);
-                    break;
-                }
-            }
-        }
-    }
-}
-
-#[derive(Debug)]
-struct TaskHub {
-    table: HashMap<usize, Task>,
-    current_id: usize,
-}
-
-#[derive(Debug, Clone)]
 pub struct Protocol {
-    capacity: usize,
-    batches: Vec<u32>,
-    path: String,
-    sender: Sender<usize>,
-    receiver: Receiver<usize>,
-    pub timeout: Duration,
-    tasks: Arc<Mutex<TaskHub>>,
+    pub stream: UnixStream,
 }
 
 impl Protocol {
-    pub fn new<'a>(batches: Vec<u32>, unix_dir: &str, capacity: usize, timeout: Duration) -> Self {
-        let (sender, receiver) = bounded::<usize>(capacity);
-        Protocol {
-            capacity,
-            batches,
-            path: unix_dir.to_string(),
-            sender,
-            receiver,
-            timeout,
-            tasks: Arc::new(Mutex::new(TaskHub {
-                table: HashMap::with_capacity(capacity),
-                current_id: 0,
-            })),
+    // TODO: Bytes facilitates zero-copy, may optimize on this
+    pub fn receive(
+        &mut self,
+        flag: &mut u16,
+        ids: &mut Vec<usize>,
+        payloads: &mut Vec<Bytes>,
+    ) -> io::Result<()> {
+        let mut flag_buf = [0; LENGTH_TASK_FLAG];
+        let mut bs_buf = [0; LENGTH_TASK_BATCH];
+        self.stream.read_exact(&mut flag_buf)?;
+        self.stream.read_exact(&mut bs_buf)?;
+        *flag = u16::from_be_bytes(flag_buf);
+        let mut bs = u16::from_be_bytes(bs_buf);
+
+        while bs > 0 {
+            bs -= 1;
+            let mut id_buf = [0; LENGTH_TASK_ID];
+            let mut len_buf = [0; LENGTH_TASK_BODY_LEN];
+            self.stream.read_exact(&mut id_buf)?;
+            self.stream.read_exact(&mut len_buf)?;
+            let id = u32::from_be_bytes(id_buf) as usize;
+            let len = u32::from_be_bytes(len_buf) as usize;
+            let mut buffer = vec![0u8; len];
+            let mut payload_buf = &mut buffer[..];
+            self.stream.read_exact(&mut payload_buf)?;
+            ids.push(id);
+            payloads.push(Bytes::from(payload_buf.to_vec()));
         }
+        Ok(())
     }
 
-    pub async fn run(&mut self) {
-        let mut last_receiver = self.receiver.clone();
-        let folder = Path::new(&self.path);
-        if !folder.is_dir() {
-            fs::create_dir(folder).unwrap();
+    pub fn send(&mut self, flag: u16, ids: &Vec<usize>, payloads: &Vec<Bytes>) -> io::Result<()> {
+        let mut buffer: Vec<u8> = Vec::new();
+        let flag_buf = u16::to_be_bytes(flag);
+        buffer.extend_from_slice(&flag_buf);
+        let bs = ids.len() as u16;
+        let bs_buf = u16::to_be_bytes(bs);
+        buffer.extend_from_slice(&bs_buf);
+        if bs > 0 {
+            for (id, payload) in ids.iter().zip(payloads.iter()) {
+                let id_buf = u32::to_be_bytes(*id as u32);
+                let len_buf = u32::to_be_bytes(payload.len() as u32);
+                buffer.extend_from_slice(&id_buf);
+                buffer.extend_from_slice(&len_buf);
+                buffer.extend_from_slice(&payload.to_vec());
+            }
         }
-
-        for (i, batch) in self.batches.iter().enumerate() {
-            let (sender, receiver) = bounded::<usize>(self.capacity);
-            let processor = Processor::new(
-                self.tasks.clone(),
-                *batch,
-                &folder.join(format!("ipc_{:?}.socket", i)),
-                last_receiver.clone(),
-                sender.clone(),
-            );
-            tokio::spawn(async move {
-                processor.run().await;
-            });
-            last_receiver = receiver.clone();
-        }
-        self.receiver = last_receiver.clone();
-    }
-
-    pub async fn add_new_task(&self, data: Bytes, notifier: oneshot::Sender<()>) -> usize {
-        let mut tasks = self.tasks.lock().await;
-        let id = tasks.current_id;
-        tasks.table.insert(id, Task::new(data, notifier));
-        let _ = tasks.current_id.wrapping_add(1);
-        id
-    }
-
-    pub async fn get_task_info(&self, id: usize) -> Info {
-        let mut tasks = self.tasks.lock().await;
-        match tasks.table.remove(&id) {
-            Some(task) => task.info.clone(),
-            None => Info {
-                code: TaskCode::UnknownError,
-                data: Bytes::from(""),
-            },
-        }
+        self.stream.write_all(&buffer[..])?;
+        Ok(())
     }
 }
