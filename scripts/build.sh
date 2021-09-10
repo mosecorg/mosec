bed93b74fb86645913cce7b77a6d97803b8bf2b3
Keming <kemingy94@gmail.com>
2021-09-10 12:56:14 +0800

    fix rustup target

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,8 +8,10 @@ for VERSION in 3.6 3.7 3.8 3.9; do
   conda activate py$VERSION
 
   pip install --no-cache-dir setuptools wheel
+  rustup target add x86_64-unknown-linux-gnu
   PRODUCTION_MODE=yes RUST_TARGET=x86_64-unknown-linux-gnu python setup.py \
     bdist_wheel --plat-name manylinux1_x86_64 # linux
+  rustup target add x86_64-apple-darwin
   PRODUCTION_MODE=yes RUST_TARGET=x86_64-apple-darwin python setup.py \
     bdist_wheel --plat-name macosx_10.9_x86_64 # macos
 
