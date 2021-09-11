58bb43c91119a77e0b983dca0de765118f5f1102
Keming <kemingy94@gmail.com>
2021-09-11 09:33:02 +0800

    use two os

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,20 +2,13 @@
 set -e
 eval "$(conda shell.bash hook)"
 
-# Download rust target
-rustup target add x86_64-unknown-linux-gnu
-rustup target add x86_64-apple-darwin
-
 # Support multiple python versions
 for VERSION in 3.6 3.7 3.8 3.9; do
   conda create -y -n py$VERSION python=$VERSION
   conda activate py$VERSION
 
   pip install --no-cache-dir setuptools wheel
-  PRODUCTION_MODE=yes RUST_TARGET=x86_64-unknown-linux-gnu python setup.py \
-    bdist_wheel --plat-name manylinux1_x86_64 # linux
-  PRODUCTION_MODE=yes RUST_TARGET=x86_64-apple-darwin python setup.py \
-    bdist_wheel --plat-name macosx_10.9_x86_64 # macos
+  PRODUCTION_MODE=yes python setup.py bdist_wheel --plat-name manylinux1_x86_64
 
   conda deactivate
   rm -rf build
