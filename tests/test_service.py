25e005714ac2a999db82792befb7154f84047c13
zclzc <38581401+lkevinzc@users.noreply.github.com>
2021-10-03 21:01:20 +0800

    add multiprocess test service (#69)
    * add multiprocess test service
    
    * add async client call
    
    * add batch size > 1 check
    
    * minor
    
    * rm async
    
    * use fixture for client
    
    * Apply suggestions from code review
    
    Co-authored-by: Keming <kemingy94@gmail.com>
    
    * naming
    
    * Update tests/test_service.py
    
    Co-authored-by: Keming <kemingy94@gmail.com>
    
    Co-authored-by: Keming <kemingy94@gmail.com>

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,5 +1,8 @@
+import random
+import re
 import subprocess
 import time
+from threading import Thread
 
 import httpx  # type: ignore
 import pytest
@@ -7,6 +10,14 @@ import pytest
 import mosec
 
 TEST_PORT = "8090"
+URI = f"http://localhost:{TEST_PORT}"
+
+
+@pytest.fixture(scope="module")
+def http_client():
+    client = httpx.Client()
+    yield client
+    client.close()
 
 
 @pytest.fixture(scope="session")
@@ -22,17 +33,44 @@ def mosec_service():
     service.terminate()
 
 
-def test_square_service(mosec_service):
-    URI = f"http://localhost:{TEST_PORT}"
-    resp = httpx.get(URI)
+def test_square_service(mosec_service, http_client):
+    resp = http_client.get(URI)
     assert resp.status_code == 200
     assert f"mosec/{mosec.__version__}" == resp.headers["server"]
 
-    resp = httpx.get(f"{URI}/metrics")
+    resp = http_client.get(f"{URI}/metrics")
     assert resp.status_code == 200
 
-    resp = httpx.post(f"{URI}/inference", json={"msg": 2})
+    resp = http_client.post(f"{URI}/inference", json={"msg": 2})
     assert resp.status_code == 422
 
-    resp = httpx.post(f"{URI}/inference", json={"x": 2})
-    assert resp.json()["x"] == 4
+    resp = http_client.post(f"{URI}/inference", content=b"bad-binary-request")
+    assert resp.status_code == 422
+
+    validate_square_service(http_client, 2)
+
+
+def test_square_service_mp(mosec_service, http_client):
+    threads = []
+    for _ in range(20):
+        t = Thread(
+            target=validate_square_service,
+            args=(http_client, random.randint(-500, 500)),
+        )
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+    assert_batch_larger_than_one(http_client)
+
+
+def validate_square_service(http_client, x):
+    resp = http_client.post(f"{URI}/inference", json={"x": x})
+    assert resp.json()["x"] == x ** 2
+
+
+def assert_batch_larger_than_one(http_client):
+    metrics = http_client.get(f"{URI}/metrics").content.decode()
+    bs = re.findall(r"batch_size_bucket.+", metrics)
+    get_bs_int = lambda x: int(x.split(" ")[-1])  # noqa
+    assert get_bs_int(bs[-1]) > get_bs_int(bs[0])
