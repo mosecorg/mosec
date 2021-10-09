5111965ba5e755ae4608dbf616937e41ae1cd609
Keming <kemingy94@gmail.com>
2021-10-10 01:16:35 +0800

    add python side metric example

---
+++ examples/python_side_metrics.py
@@ -0,0 +1,84 @@
+import os
+import pathlib
+import tempfile
+import threading
+import logging
+from wsgiref.simple_server import make_server
+from typing import List, Any
+
+from mosec import Server, Worker
+from mosec.errors import ValidationError
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    "%(asctime)s - %(process)d - %(levelname)s - %(filename)s:%(lineno)s - %(message)s"
+)
+sh = logging.StreamHandler()
+sh.setFormatter(formatter)
+logger.addHandler(sh)
+
+
+# check the PROMETHEUS_MULTIPROC_DIR environment variable before import Prometheus
+if not os.environ.get("PROMETHEUS_MULTIPROC_DIR"):
+    metric_dir_path = os.path.join(tempfile.gettempdir(), "prometheus_multiproc_dir")
+    pathlib.Path(metric_dir_path).mkdir(parents=True, exist_ok=True)
+    os.environ["PROMETHEUS_MULTIPROC_DIR"] = metric_dir_path
+
+from prometheus_client import (
+    multiprocess,
+    generate_latest,
+    CollectorRegistry,
+    CONTENT_TYPE_LATEST,
+    Counter,
+)
+
+metric_registry = CollectorRegistry()
+multiprocess.MultiProcessCollector(metric_registry)
+counter = Counter("inference_result", "statistic of result", ("status", "pid"))
+
+
+def metric_app(environ, start_response):
+    data = generate_latest(metric_registry)
+    start_response(
+        "200 OK",
+        [("Content-Type", CONTENT_TYPE_LATEST), ("Content-Length", str(len(data)))],
+    )
+    return iter([data])
+
+
+def metric_service(host="", port=8080):
+    with make_server(host, port, metric_app) as httpd:
+        httpd.serve_forever()
+
+
+class Inference(Worker):
+    def deserialize(self, data: bytes) -> Any:
+        data = super().deserialize(data)
+        try:
+            data = int(data.get("num"))
+        except AttributeError:
+            raise ValidationError("data must be int type")
+        return data
+
+    def forward(self, data: List[int]) -> List[bool]:
+        avg = sum(data) / len(data)
+        ans = [x >= avg for x in data]
+        pid = str(os.getpid())
+        counter.labels(status="true", pid=pid).inc(sum(ans))
+        counter.labels(status="false", pid=pid).inc(len(ans) - sum(ans))
+        return ans
+
+
+if __name__ == "__main__":
+    # Run the metrics server in another thread.
+    # This won't affect the inference performance because most of the computations
+    # are done in the other processes. The main process is only on behalf of the
+    # controller and metrics.
+    metric_thread = threading.Thread(target=metric_service, daemon=True)
+    metric_thread.start()
+
+    # Run the inference server
+    server = Server()
+    server.append_worker(Inference, max_batch_size=8)
+    server.run()
