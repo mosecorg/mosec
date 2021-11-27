5fdd0547c3bf32dede9160b1d5f5de93ab159eb0
zclzc <38581401+lkevinzc@users.noreply.github.com>
2021-11-27 21:22:22 +0800

    fix doc-gen

--- scripts/gen_docs_args.py
+++ scripts/gen_docs_args.py
@@ -6,7 +6,7 @@ process = subprocess.Popen(
     ["python", "mosec/args.py", "--help"], stdout=subprocess.PIPE
 )
 stdout = process.communicate()[0].decode()
-stdout = stdout.replace("args.py", "your_model_server.py")
+stdout = stdout.replace("args.py", "python your_model_server.py")
 mkstr = f"```text\n{stdout}\n```"
 
 with mkdocs_gen_files.open("argument.md", "w") as f:
