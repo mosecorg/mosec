c0f3fbc7692cdbf68c79e8d4894b21ca79bb2461
Zichen Liu <lkevinzc@gmail.com>
2021-12-13 14:51:33 +0800

    endec wrapper

--- mosec/shm.py
+++ mosec/shm.py
@@ -1,4 +1,5 @@
-from typing import Any, List
+import pickle
+from typing import Any, Callable, List
 
 try:
     from pyarrow import plasma  # type: ignore
@@ -28,3 +29,39 @@ class ShmClient:
         objects = self.client.get(object_ids)
         self.client.delete(object_ids)
         return objects
+
+
+class ShmWrapper:
+    def __init__(
+        self,
+        shm_cli: ShmClient,
+        serializer: Callable = pickle.dumps,
+        deserializer: Callable = pickle.loads,
+    ) -> None:
+        """Wrap the shm plugin for protocol.
+
+        Args:
+            serializer (Callable, optional): serializer before put.
+                Defaults to pickle.dumps.
+            deserializer (Callable, optional): deserializer after get.
+                Defaults to pickle.loads.
+        """
+        self.shm_cli = shm_cli
+        self.serializer = serializer
+        self.deserializer = deserializer
+
+    def wrap_decoder(self, decoder: Callable) -> Callable:
+        def wrapped(batch_bytes: List[bytes]) -> List[Any]:
+            batch_ids = [decoder(x) for x in batch_bytes]
+            batch_data = self.shm_cli.get(batch_ids)
+            return [self.deserializer(x) for x in batch_data]
+
+        return wrapped
+
+    def wrap_encoder(self, encoder: Callable) -> Callable:
+        def wrapped(data: Any) -> bytes:
+            data_bytes = encoder(data)
+            object_id = self.shm_cli.put(data_bytes)
+            return self.serializer(object_id)
+
+        return wrapped
--- tests/square_service_shm.py
+++ tests/square_service_shm.py
@@ -13,7 +13,15 @@ class SquareService(Worker):
         return result
 
 
+class DummyPostprocess(Worker):
+    """This dummy stage is added to test the shm IPC"""
+
+    def forward(self, data: dict) -> dict:
+        return data
+
+
 if __name__ == "__main__":
     server = Server(plasma_shm=1000 * 1000 * 20)
     server.append_worker(SquareService, max_batch_size=8)
+    server.append_worker(DummyPostprocess, num=2)
     server.run()
