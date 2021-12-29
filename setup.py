811d2257ae5d3434572fb7190ac6d14091e56ad6
Keming <kemingy94@gmail.com>
2021-12-29 10:13:54 +0800

    fix test

--- setup.py
+++ setup.py
@@ -107,7 +107,6 @@ setup(
             "msgpack>=1.0.2",
             "pre-commit>=2.15.0",
             "httpx>=0.19.0",
-            "pyarrow>=6.0.1",
         ],
         "doc": [
             "mkdocstrings>=0.16.0",
