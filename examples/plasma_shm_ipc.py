9a3f183b3280aa03ae759e851ae5f1807611a992
thinkcache <aniket1shirsat@gmail.com>
2022-07-03 11:52:34 +0530

    fix lint plasm shm ipc example

--- examples/plasma_shm_ipc.py
+++ examples/plasma_shm_ipc.py
@@ -12,6 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Example: Using Plasma store with mosec plugin PlasmaShmWrapper.
+
+We start a subprocess for the plasma server, and pass the path
+to the plasma client which serves as the shm wrapper.
+We also register the plasma server process as a daemon, so
+that when it exits the service is able to gracefully shutdown
+and restarted by the orchestrator.
+"""
+
 from functools import partial
 
 from pyarrow import plasma  # type: ignore
@@ -22,27 +31,27 @@ from mosec.plugins import PlasmaShmWrapper
 
 
 class DataProducer(Worker):
+    """Sample Data Producer."""
+
     def forward(self, data: dict) -> bytes:
+        """Need help here."""
         try:
             data_bytes = b"a" * int(data["size"])
         except KeyError as err:
-            raise ValidationError(err)
+            raise ValidationError(err) from err
         return data_bytes
 
 
 class DataConsumer(Worker):
+    """Sample Data Consumer."""
+
     def forward(self, data: bytes) -> dict:
+        """Need help here."""
         return {"ipc test data length": len(data)}
 
 
 if __name__ == "__main__":
-    """
-    We start a subprocess for the plasma server, and pass the path
-    to the plasma client which serves as the shm wrapper.
-    We also register the plasma server process as a daemon, so
-    that when it exits the service is able to gracefully shutdown
-    and restarted by the orchestrator.
-    """
+
     # 200 Mb store, adjust the size according to your requirement
     with plasma.start_plasma_store(plasma_store_memory=200 * 1000 * 1000) as (
         shm_path,
