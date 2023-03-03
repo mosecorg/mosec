2d99dae252b55aa9220158e085d0d1bc686f1762
Keming <kemingy94@gmail.com>
2023-03-04 00:33:05 +0800

    feat: support set up the arguments through environment variables (#301)
    Signed-off-by: Keming <kemingy94@gmail.com>

--- mosec/env.py
+++ mosec/env.py
@@ -18,7 +18,21 @@ from __future__ import annotations
 
 import contextlib
 import os
+import warnings
+from argparse import Namespace
 from typing import Dict, List
+
+MOSEC_ENV_PREFIX = "MOSEC_"
+MOSEC_ENV_CONFIG = {
+    "path": str,
+    "capacity": int,
+    "timeout": int,
+    "address": str,
+    "port": int,
+    "namespace": str,
+    "debug": bool,
+    "dry_run": bool,
+}
 
 
 @contextlib.contextmanager
@@ -34,3 +48,27 @@ def env_var_context(env: None | List[Dict[str, str]], index: int):
     finally:
         for key, value in default.items():
             os.environ[key] = value
+
+
+def get_env_namespace(prefix: str = MOSEC_ENV_PREFIX) -> Namespace:
+    """Read the config from environment variables before the argument parsing.
+
+    Priority: CLI > env > default value.
+    """
+    namespace = Namespace()
+    for name, converter in MOSEC_ENV_CONFIG.items():
+        var = f"{prefix}{name.upper()}"
+        value = os.environ.get(var)
+        if not value:
+            continue
+        try:
+            val = converter(value)
+        except ValueError as err:
+            warnings.warn(
+                f"failed to convert env {var}={value} to type {converter} {err}, "
+                "will skip this one",
+            )
+        else:
+            setattr(namespace, name, val)
+
+    return namespace
