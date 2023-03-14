4a123471d60d32f1199b22c1f09c4f2291d21bb4
Keming <kemingy94@gmail.com>
2023-03-14 22:59:16 +0800

    fix: align warmup example format in dry run
    Signed-off-by: Keming <kemingy94@gmail.com>

--- examples/echo.py
+++ examples/echo.py
@@ -24,7 +24,7 @@ logger = get_logger()
 class Preprocess(Worker):
     """Sample Class."""
 
-    example = [{"time": 0}]
+    example = {"time": 0}
 
     def forward(self, data: dict) -> float:
         logger.debug("pre received %s", data)
@@ -38,6 +38,8 @@ class Preprocess(Worker):
 
 class Inference(Worker):
     """Sample Class."""
+
+    example = [0, 1e-5, 2e-4]
 
     def forward(self, data: List[float]) -> List[float]:
         logger.info("sleeping for %s seconds", max(data))
