e791b5d2630f863c881f2730f6f9233ea50a7b45
Liu Zichen <lkevinzc@gmail.com>
2023-05-23 11:11:54 +0800

    better logo for night mode

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,13 +46,13 @@ napoleon_include_init_with_doc = True
 napoleon_use_admonition_for_references = True
 # https://sphinxext-opengraph.readthedocs.io/en/latest/
 ogp_site_url = "https://mosecorg.github.io/mosec/"
-ogp_image = "https://user-images.githubusercontent.com/38581401/134487662-49733d45-2ba0-4c19-aa07-1f43fd35c453.png"
+ogp_image = "https://user-images.githubusercontent.com/38581401/240107101-6949eb5b-72a5-4084-a043-671ba1e02594.png"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_logo = "https://user-images.githubusercontent.com/38581401/134487662-49733d45-2ba0-4c19-aa07-1f43fd35c453.png"
+html_logo = "https://user-images.githubusercontent.com/38581401/240107101-6949eb5b-72a5-4084-a043-671ba1e02594.png"
 html_static_path = ["_static"]
 html_favicon = "https://user-images.githubusercontent.com/38581401/134798617-0104dc12-e0d4-4ed5-a79c-9e2435e99a14.png"
 
